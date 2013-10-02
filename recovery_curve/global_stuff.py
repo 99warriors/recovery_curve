98e038e4f0ea863e8cdea9ecb4d88a9f8f95d374
glareprotector <glareprotector@glareprotectors-MacBook-Air.local>
2013-10-02 10:55:28 -0400

    added global_stuff

---
+++ recovery_curve/global_stuff.py
@@ -0,0 +1,8 @@
+home = '/Users/glareprotector/Documents/lab/glare_remix2'
+data_home = '%s/%s/%s' % (home, 'bin', 'cache')
+train_diffcovs_r_script = '%s/%s/%s' % (home, 'recovery_curve', 'train_diffcovs_model.r')
+raw_data_home = '%s/%s' % (home, 'raw_data')
+xs_file = '%s/%s' % (raw_data_home, 'xs.csv')
+ys_folder = '%s/%s' % (raw_data_home, 'series')
+all_pid_file = '%s/%s' % (raw_data_home, 'pids.csv')
+
