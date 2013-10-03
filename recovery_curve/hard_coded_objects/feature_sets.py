00a58dac35f34ee50febc6ae07c3bf7e2751bad6
glareprotector <glareprotector@glareprotectors-MacBook-Air.local>
2013-10-03 12:56:46 -0400

    added some hardcoded features for iterating over pairs of age/initial values

--- recovery_curve/hard_coded_objects/feature_sets.py
+++ recovery_curve/hard_coded_objects/feature_sets.py
@@ -1,4 +1,12 @@
-import recovery_curve.prostate_specifics as ps
+from recovery_curve.prostate_specifics import *
 from recovery_curve.management_stuff import *
 
-a_feature_set = set_hard_coded_key_dec(ps.keyed_list, 'a_feature_set')([ucla_cov_f(ucla_cov_f.age), bin_f(ucla_cov_f(ucla_cov_f.psa), bin(0,20)), s_f(ys_f(ys_f.sexual_function)), ones_f()])
+
+
+default_simple_indicators = set_hard_coded_key_dec(keyed_list, 'default_simple_indicators')(get_categorical_fs(ucla_cov_f(ucla_cov_f.age), [bin(None,55), bin(55,65), bin(65,None)]) + get_categorical_fs(s_f(ys_f(ys_f.sexual_function)), [bin(0,0.41), bin(0.41,0.6), bin(0.6,0.8), bin(0.8,None)]) + [ones_f()])
+
+default_age_categorical_f = set_hard_coded_key_dec(get_categorical_fs,'dfltage')(ucla_cov_f(ucla_cov_f.age), [bin(None,55), bin(55,65), bin(65,None)])
+medium_age_categorical_f = set_hard_coded_key_dec(get_categorical_fs,'medtage')(ucla_cov_f(ucla_cov_f.age), [bin(None,55), bin(55,60), bin(60,65), bin(65,None)])
+
+default_initial_categorical_f = set_hard_coded_key_dec(get_categorical_fs,'dfltinit')(s_f(ys_f(ys_f.sexual_function)), [bin(0,0.41), bin(0.41,0.6), bin(0.6,0.8), bin(0.8,None)])
+high_initial_categorical_f = set_hard_coded_key_dec(get_categorical_fs,'hiinit')(s_f(ys_f(ys_f.sexual_function)), [bin(0,0.41), bin(0.41,0.6), bin(0.6,0.7), bin(0.7,0.8), bin(0.8,0.9), bin(0.9,1.0)])
--- recovery_curve/hard_coded_objects/hypers.py
+++ recovery_curve/hard_coded_objects/hypers.py
@@ -1,4 +1,6 @@
 import recovery_curve.prostate_specifics as ps
 from recovery_curve.management_stuff import *
 
-a_hyper = set_hard_coded_key_dec(ps.hypers, 'a_hyper')(1.0,1.0,1.0,15.0,15.0,15.0,10.0)  
+default_hyper = set_hard_coded_key_dec(ps.hypers, 'dflthyper')(1.0,1.0,1.0,15.0,15.0,15.0,10.0)  
+medium_hyper = set_hard_coded_key_dec(ps.hypers, 'medhyper')(0.5,0.5,0.5,15.0,15.0,15.0,10.0)  
+relaxed_hyper = set_hard_coded_key_dec(ps.hypers, 'relhyper')(1.0,1.0,1.0,5.0,5.0,5.0,10.0)  
