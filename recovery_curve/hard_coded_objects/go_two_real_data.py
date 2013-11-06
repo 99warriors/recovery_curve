f8be7e70bf0a9dc1b39a7955b77f3215f7f5031d
glareprotector <glareprotector@glareprotectors-MacBook-Air.local>
2013-11-05 20:36:27 -0500

    ?

--- recovery_curve/hard_coded_objects/go_two_real_data.py
+++ recovery_curve/hard_coded_objects/go_two_real_data.py
@@ -4,7 +4,7 @@ import recovery_curve.hard_coded_objects.feature_sets as hard_coded_feature_sets
 
 upscale_delta = 0.01
 post_process_f = ps.normalized_data_f()
-
+feature_set = hard_coded_feature_sets.default_simple_indicators
 
 pid_iterator = gd.filtered_pid_iterator(set_hard_coded_key_dec(gd.filtered_pid_iterator,'surgpids')(gd.all_ucla_pid_iterator(), gd.bin_f(ps.ucla_treatment_f(),ps.equals_bin([gd.ucla_treatment_f.surgery]))), gd.is_good_pid())
 
@@ -12,7 +12,7 @@ ys_f = gd.modified_ys_f(gd.ys_f(gd.ys_f.sexual_function), gd.push_0_up_ys_modifi
 init_f = gd.set_hard_coded_key_dec(gd.s_f, 'init')(ys_f)
 actual_ys_f = gd.actual_ys_f(ys_f, actual_ys_f_shift)
 
-feature_set = hard_coded_feature_sets.default_simple_indicators
+
 x_abc_fs = set_hard_coded_key_dec(ps.x_abc_fs, feature_set.get_key())(feature_set, feature_set, feature_set)
 
 
