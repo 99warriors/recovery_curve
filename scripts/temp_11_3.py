daa05b263858231cec9a289f6fe1e7ef8d2368e4
Ubuntu <ubuntu@ip-172-31-8-48.us-west-2.compute.internal>
2013-11-07 13:46:46 +0000

    g

--- scripts/temp_11_3.py
+++ scripts/temp_11_3.py
@@ -15,44 +15,16 @@ goal of this script is to see self_cv results
 
 
 data = importlib.import_module(sys.argv[1]).data
-get_posterior_f = importlib.import_module(sys.argv[2]).get_posterior_f
-posteriors = get_posterior_f(data)
+get_posterior_f_module = importlib.import_module(sys.argv[2])
+get_posterior_f = get_posterior_f_module.get_posterior_f
+cv_f = ps.cv_fold_f(4)
 
 
-"""
-below stuff is all just plotting
-"""
+for train_data, test_data in cv_f(data):
+    try:
+        posterior = get_posterior_f(train_data, test_data)
+    except TypeError:
+        posterior = get_posterior_f(train_data)
 
 
-times = [1,2,4,8,12,18,24,30,36,42,48]
-cv_f = ps.self_test_cv_f()
 
-pointwise_mean_trainer = pt.t_point_trainer_from_t_distribution_trainer(pt.t_distribution_trainer_from_abc_distribution_trainer(pt.builtin_auto_abc_distribution_trainer(get_posterior_f)), ps.mean_f())
-abc_mean_trainer = pt.t_point_trainer_from_abc_point_trainer(pt.abc_point_trainer_from_abc_distribution_trainer(pt.builtin_auto_abc_distribution_trainer(get_posterior_f), ps.mean_f()))
-logreg_trainer = pt.logreg_trainer(times)
-logreg_shape_trainer = pt.logregshape_trainer(times)
-
-from recovery_curve.hard_coded_objects.truncated_normal_regular_get_posterior_f import get_posterior_f as truncated_normal_get_posterior_f
-from recovery_curve.hard_coded_objects.beta_regular_get_posterior_f import get_posterior_f as beta_get_posterior_f
-
-trainer_plotter_cons_tuples = [\
-#    (pt.builtin_auto_abc_distribution_trainer(truncated_normal_get_posterior_f), ps.keyed_partial(plotters.abc_distribution_predictor_curve_plotter, 0.05, 150, 0, 50, 500, 'cyan', 'normal')), \
-    (pt.builtin_auto_abc_distribution_trainer(beta_get_posterior_f), ps.keyed_partial(plotters.abc_distribution_predictor_curve_plotter, 0.05, 150, 0, 50, 500, 'red', 'beta')), \
-        (pt.t_point_trainer_from_abc_point_trainer(pt.abc_point_trainer_from_abc_distribution_trainer(pt.builtin_auto_abc_distribution_trainer(truncated_normal_get_posterior_f), ps.mean_f())), ps.keyed_partial(plotters.t_point_predictor_curve_plotter, 0,50,150,'orange','normal_pointwise_mean')), \
-        (pt.t_point_trainer_from_abc_point_trainer(pt.abc_point_trainer_from_abc_distribution_trainer(pt.builtin_auto_abc_distribution_trainer(beta_get_posterior_f), ps.mean_f())), ps.keyed_partial(plotters.t_point_predictor_curve_plotter, 0,50,150,'green','beta_pointwise_mean')), \
-    ]
-try:
-    posteriors = get_posterior_f(data)
-    #stats = ps.gelman_statistic_f()(posteriors)
-    #ps.plot_single_posterior_f(3,2)(posteriors)
-
-    ps.plot_all_predictions_fig_f(trainer_plotter_cons_tuples, cv_f)(data)
-
-    loss_f = ps.loss_f(ps.get_true_val(), ps.scaled_logistic_loss_f(10))
-    percentiles = [.25,.50,.75]
-    trainers = [pointwise_mean_trainer, abc_mean_trainer, logreg_trainer, logreg_shape_trainer]
-    
-    ps.model_comparer_f(trainers, cv_f, loss_f, percentiles, times)
-except Exception, e:
-    print e
-    ps.print_traceback()
